75d061c93bb6efa28981502496f3f391214e7449
ashkanbossani <abossani@hotmail.com>
2022-07-18 15:53:48 -0700

    Intial folder structer set up for server side

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,4 +3,22 @@ const router = express.Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
+let inventoryList = [];
+
+fs.readFile("./data/inventories.json", "utf8", (err, data) => {
+  err ? console.log(err) : (inventoryList = JSON.parse(data));
+});
+
+router.get("/", (req, res) => {
+  res.status(200).json(inventoryList);
+});
+
+router.get("/:id", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+router.post("/add", (req, res) => {});
+
 module.exports = router;
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -3,4 +3,28 @@ const router = express.Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
+router.use(express.json());
+
+let warehouseList = [];
+
+fs.readFilee("./data/warehouses.json", "utf8", (err, data) => {
+  err ? console.log(err) : (warehouseList = JSON.parse(data));
+});
+
+router.get("/", (req, res) => {
+  res.status(200).json(warehouseList);
+});
+
+router.get("/", (req, res) => {
+  //get request for mapped warehouselist for certain info in warehouse data
+});
+
+router.get("/:id", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+router.post("/add", (req, res) => {});
+
 module.exports = router;
